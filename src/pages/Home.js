dc1bb8667a38f6f330b5b81732862973c380cb99
Lithelli <leepatner@ymail.com>
2019-04-08 15:55:38 -0500

    fesh

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,44 @@
 import React, { Component } from 'react'
-import { Text, View, SafeAreaView } from 'react-native'
+import { Text, View, SafeAreaView, StyleSheet } from 'react-native'
 import { Button } from 'react-native-elements'
 
 export default class Home extends Component {
   render() {
     return (
-      <SafeAreaView>
-        <Button>Eat</Button>
-        <Button>Do</Button>
-        <Button>See</Button>
-        <Button>Drink</Button>
+      <SafeAreaView style={styles.container}>
+        <Button
+        buttonStyle= {styles.button}
+        title="Eat"
+        raised = {true}
+        />
+        <Button
+        buttonStyle= {styles.button}
+        title="Do"
+        raised = {true}
+        />
+        <Button
+        buttonStyle= {styles.button}
+        title="See"
+        raised = {true}
+        />
+        <Button
+        buttonStyle= {styles.button}
+        title="Drink"
+        raised = {true}
+        />
       </SafeAreaView>
     )
   }
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-evenly',
+  },
+  button: {
+    width: 200,
+    height: 50,
+  }
+})
---
+++ src/pages/Results.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import { Text, SafeAreaView, ImageBackground, StyleSheet, ScrollView, View } from 'react-native'
+
+export default class Results extends Component {
+  render() {
+    return (
+    <ScrollView>
+      <SafeAreaView>
+            <ImageBackground style={styles.resultC}>
+                <Text style={styles.Name}>
+                    Name
+                </Text>
+                <Text style={styles.Type}>
+                    Type
+                </Text>
+            </ImageBackground>
+          {/* This will be a map of the results from the parent choice */}
+        </SafeAreaView>
+    </ScrollView>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+    resultC:{
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-around",
+        alignItems: "center",
+        borderBottomWidth: 1,
+        backgroundColor: "red",
+        width: "100%",
+        height: "100%",
+        paddingTop: 20,
+        paddingBottom: 20,
+    },
+    Name:{
+        fontSize: 40,
+    },
+    Type:{
+        fontSize: 30,
+    }
+})
