99b16d54ebd77a7e4b30610c8e94fef5c13a2fe8
Lithelli <leepatner@ymail.com>
2019-04-08 21:07:46 -0500

    here ya go

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,68 +1,51 @@
 import React, { Component } from 'react'
-import { View, SafeAreaView, Image, StyleSheet } from 'react-native'
-import { Button, Text } from 'react-native-elements';
+import { View, SafeAreaView, StyleSheet } from 'react-native'
+import { Text } from 'react-native-elements'
 
 export default class Details extends Component {
   render() {
     return (
-        <SafeAreaView>
-            <View style={styles.UserInfo}>
-                <View style={styles.Name}>
-                    <Text style={{fontSize: 35}}>Name</Text>
-                    <Text style={{fontSize: 30}}>Email</Text>
-                </View>
-                <View style={styles.Image}>
-                    <Image
-                    style = {{width: 100, height: 100}}
-                    borderRadius= {50}
-                    source={{uri: "https://411mania.com/wp-content/uploads/2018/04/John-Cena-Raw-4218-645x370.jpg"}}/>
-                </View>
+      <SafeAreaView style={styles.Container}>
+        <View style={styles.Left}>
+            <View style={styles.Top}>
+                <Text h3>Name</Text>
+                <Text h4>Type</Text>
             </View>
-            <View style={styles.UserContent}>
-                <View style={styles.Reviews}>
-                    <Text h3 style={{paddingBottom: 20}}>My Reviews</Text>
-                    {/* HardCode or Map. Probably Hard Code */}
-                    <Text>YUM</Text>
-                    <Text>Not YUM</Text>
-                </View>
-                <View style={styles.FoodTruck}>
-                    <Button
-                    title = "Add Food Truck"
-                    raised = {true}
-                    />
-                </View>
+            <View style={styles.Bottom}>
+                {/* this is where the details are mapped through per place */}
+                <Text>Address</Text>
+                <Text>Phone</Text>
+                <Text>Hours</Text>
+                <Text>Website</Text>
             </View>
-        </SafeAreaView>
+            
+        </View>
+        
+        <View style={styles.Right}>
+            <Text>
+                test
+            </Text>
+        </View>
+      </SafeAreaView>
     )
   }
 }
-
 const styles = StyleSheet.create({
-    UserInfo:{
-      flexDirection:"row",
-      borderBottomWidth: 1,
-      marginBottom: 20
-    },
-    Name:{
-        flex:1,
-        alignItems: "center"
-    },
-    Image:{
-        flex:1,
-        alignItems: "flex-end",
-        paddingRight: 20,
-    },
-    UserContent:{
-        flexDirection:"row",
-        height: "75%"
-    },
-    Reviews:{
-        flex:1,
+    Left:{
+        flexDirection: "column",
+        backgroundColor:"whitesmoke",
+        justifyContent: "space-between",
         alignItems:"center",
+        width:"50%",
+        height: "100%"
     },
-    FoodTruck:{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-    }
+    Right:{
+        flexDirection: "column",
+        backgroundColor: "grey",
+        width:"50%",
+    },
+    Container:{
+        flexDirection:"row",
+        height:"100%"
+    },
 })
