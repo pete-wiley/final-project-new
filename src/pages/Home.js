1a4b3a1e98f1ea97969bf2a7498f1cfc34ca451b
alec-justice <alec.justice2017@gmail.com>
2019-04-11 10:16:34 -0500

    just a pull

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Text, View, SafeAreaView, StyleSheet } from 'react-native'
 import { Button, ListItem } from 'react-native-elements'
 import  Icon  from 'react-native-vector-icons/FontAwesome';
+import {BoxShadow} from 'react-native-shadow'
 
 
 
@@ -24,11 +25,13 @@ export default class Home extends Component {
         <Text style={styles.banner}>
           What do you want to do?
         </Text>
-        <ListItem
-        containerStyle = {styles.ListItem}
-        title="Eat"
-        onPress={() => this.props.navigation.navigate('EatResults')}
-        />
+        <BoxShadow style={styles.shadowOpt}>
+          <ListItem
+          containerStyle = {styles.ListItem}
+          title="Eat"
+          onPress={() => this.props.navigation.navigate('EatResults')}
+          />
+        </BoxShadow>
         <ListItem
         containerStyle= {styles.ListItem}
         title="Do"
@@ -57,10 +60,10 @@ const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
     justifyContent: 'space-evenly',
-    height: '85%'
+    height: 85
   },
   ListItem: {
-    width: "100%",
+    width: 100,
     height: 144,
     // borderBottomWidth: 1,
     margin: 0,
@@ -70,5 +73,8 @@ const styles = StyleSheet.create({
   banner: {
     fontSize: 35,
     alignItems: 'center'
-  }
+  },
+  shadowOpt: {
+    
+  }  
 })
