7493e569661ffa286c99bfb7bbe3dcb69860e76d
Lithelli <leepatner@ymail.com>
2019-04-11 18:36:49 -0500

    fa

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -20,30 +20,31 @@ export default class Profile extends Component {
     return (
         <SafeAreaView>
             <View style={styles.UserInfo}>
-                <View style={styles.Name}>
-                    <Text style={{fontSize: 35}}>Name</Text>
-                    <Text style={{fontSize: 30}}>Email</Text>
-                </View>
                 <View style={styles.Image}>
                     <Image
                     style = {{width: 100, height: 100}}
                     borderRadius= {50}
                     source={{uri: "https://411mania.com/wp-content/uploads/2018/04/John-Cena-Raw-4218-645x370.jpg"}}/>
                 </View>
+                <View style={styles.Name}>
+                    <Text style={{fontSize: 35}}>John Cena</Text>
+                    <Text style={{fontSize: 16}}>Johnnie@gmail.com</Text>
+                </View>
             </View>
             <View style={styles.UserContent}>
+            <View style={styles.FoodTruck}>
+                    <Button
+                    title = "Add Food Truck"
+                    raised = {true}
+                    />
+                </View>
                 <View style={styles.Reviews}>
                     <Text h3 style={{paddingBottom: 20}}>My Reviews</Text>
                     {/* HardCode or Map. Probably Hard Code */}
                     <Text>YUM</Text>
                     <Text>Not YUM</Text>
                 </View>
-                <View style={styles.FoodTruck}>
-                    <Button
-                    title = "Add Food Truck"
-                    raised = {true}
-                    />
-                </View>
+                
             </View>
         </SafeAreaView>
     )
@@ -52,30 +53,41 @@ export default class Profile extends Component {
 
 const styles = StyleSheet.create({
     UserInfo:{
-      flexDirection:"row",
+      flexDirection:"column",
       borderBottomWidth: 1,
-      marginBottom: 20
+      marginBottom: 20,
+      paddingRight: 3,
+      paddingLeft: 3,
+      paddingTop: 20,
+      paddingBottom: 20,
+      backgroundColor: "#BDBBB6"
     },
     Name:{
-        flex:1,
-        alignItems: "center"
+        // flex:1,
+        alignItems: "center",
+        // paddingRight: 15,
+        marginBottom: 4,
+        justifyContent: "center"
     },
     Image:{
-        flex:1,
-        alignItems: "flex-end",
-        paddingRight: 20,
+        // flex:1,
+        alignItems: "center",
+        // paddingRight: 10,
+        justifyContent: "center"
     },
     UserContent:{
-        flexDirection:"row",
+        flexDirection:"column",
         height: "75%"
     },
     Reviews:{
         flex:1,
         alignItems:"center",
+        justifyContent: "flex-start"
     },
     FoodTruck:{
-        flex: 1,
+        // flex: 1,
         alignItems: "center",
-        justifyContent: "center",
+        justifyContent: "flex-start",
+        marginBottom: 40,
     }
 })
