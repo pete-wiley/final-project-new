5f22cbd64690d5a621d2e23bb978491dc015bcd2
alec-justice <alec.justice2017@gmail.com>
2019-04-15 15:54:22 -0500

    styles

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,34 +1,34 @@
-import React, { Component } from 'react'
-import  Icon  from 'react-native-vector-icons/FontAwesome';
+// import React, { Component } from 'react'
+// import  Icon  from 'react-native-vector-icons/FontAwesome';
 
-import MapView from 'react-native-maps'
+// import MapView from 'react-native-maps'
 
-export default class App extends Component {
-  static navigationOptions = ({ navigation }) => {
-    return {
-        headerBackTitle: null,
-        headerLeft:
-                <Icon
-                    name="bars"
-                    size={30}
-                    style={{ paddingLeft: 10 }}
-                    onPress={() => navigation.openDrawer()} />,
-        title: "Map"
-    }
-}
-  render() {
-    return (
-      <MapView
-        style={{flex: 1}}
-        region={{
-          latitude: 33.5186,
-          longitude: -86.8104,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421
-        }}
-        showsUserLocation={true}
-      />
-    );
-  }
-}
+// export default class App extends Component {
+//   static navigationOptions = ({ navigation }) => {
+//     return {
+//         headerBackTitle: null,
+//         headerLeft:
+//                 <Icon
+//                     name="bars"
+//                     size={30}
+//                     style={{ paddingLeft: 10 }}
+//                     onPress={() => navigation.openDrawer()} />,
+//         title: "Map"
+//     }
+// }
+//   render() {
+//     return (
+//       <MapView
+//         style={{flex: 1}}
+//         region={{
+//           latitude: 33.5186,
+//           longitude: -86.8104,
+//           latitudeDelta: 0.0922,
+//           longitudeDelta: 0.0421
+//         }}
+//         showsUserLocation={true}
+//       />
+//     );
+//   }
+// }
 
