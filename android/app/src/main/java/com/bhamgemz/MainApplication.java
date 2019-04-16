e72eaabb0b4cb3f575201f718bcf038daeed8724
Lithelli <leepatner@ymail.com>
2019-04-15 22:15:53 -0500

    fa

--- android/app/src/main/java/com/bhamgemz/MainApplication.java
+++ android/app/src/main/java/com/bhamgemz/MainApplication.java
@@ -3,6 +3,8 @@ package com.bhamgemz;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -29,6 +31,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SvgPackage(),
+            new LinearGradientPackage(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage()
       );
