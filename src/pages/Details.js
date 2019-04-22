85fc14f23b1e0e76432673ec91892f50fb023a1d
Lithelli <leepatner@ymail.com>
2019-04-22 16:14:46 -0500

    picker

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -101,16 +101,6 @@ export default class Details extends Component {
                             title={l.title}
                             rightTitle={
                                 <Rating
-<<<<<<< HEAD
-                            type='custom'
-                            ratingImage={Gem}
-                            ratingCount={l.gems}
-                            ratingTextColor='lightblue'
-                            ratingColor=''
-                            imageSize={30}
-                            onFinishRating={this.ratingCompleted}
-                            />
-=======
                                 type='custom'
                                 ratingImage={Gem}
                                 ratingCount={l.gems}
@@ -119,7 +109,6 @@ export default class Details extends Component {
                                 imageSize={30}
                                 onFinishRating={this.ratingCompleted}
                                 />
->>>>>>> 9b2962a160520b2dd2521d90885f78869a4fa4cb
                             }
                             rightSubtitle={l.reviewer}
                             titleStyle={{
